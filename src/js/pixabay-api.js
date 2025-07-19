3e1e14af8e7cf6079cea1cbd69dd2a0245b9a796
Fed Kon <fedkon96@gmail.com>
2025-07-20 00:03:21 +0300

    finish

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,23 +1,20 @@
+import axios from 'axios';
+
 export function getImagesByQuery(query) {
   const BASE_URL = 'https://pixabay.com/api/';
   const API_KEY = '51363368-0cc0b280f35048ea67ead6bf3';
 
-  const params = new URLSearchParams({
+  const params = {
     key: API_KEY,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
     per_page: 9,
-  });
+  };
 
-  return fetch(`${BASE_URL}?${params}`)
-    .then(res => {
-      if (!res.ok) {
-        throw new Error(res.status);
-      }
-      return res.json();
-    })
+  return axios(BASE_URL, { params })
+    .then(res => res.data)
     .catch(err => {
       throw err;
     });
